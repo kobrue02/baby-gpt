3388606b8d940d4988c2aaca3e46ceb244975d48
Konrad-rudolf Brueggemann <konrad@tesniere.sfs.uni-tuebingen.de>
2025-09-25 20:23:28 +0200

    merge

--- data/load_datasets.py
+++ data/load_datasets.py
@@ -24,10 +24,26 @@ def load_finepdfs(n_rows):
     })
     """
     # Login using e.g. `huggingface-cli login` to access this dataset
-    streamed = load_dataset("HuggingFaceFW/finepdfs", "eng_Latn", streaming=True, split="train")
-    ds = streamed_dataset_to_split(streamed, n_rows=n_rows, test_size=0.2)  # use a small subset for testing
+    #streamed = load_dataset("HuggingFaceFW/finepdfs", "eng_Latn", streaming=True, split="train")
+    #ds = streamed_dataset_to_split(streamed, n_rows=n_rows, test_size=0.2)  # use a small subset for testing
+    ds = load_dataset("roneneldan/TinyStories")
+    # map this to a Dataset of this shape:
+    # DatasetDict({
+    #     train: Dataset({
+    #         features: ['text'],
+    #         num_rows: 8009762
+    #     })
+    #     val: Dataset({
+    #         features: ['text'],
+    #         num_rows: 4007
+    #     })
+    # })
+
     ds = ds.remove_columns([col for col in ds['train'].column_names if col != 'text']) # type: ignore
-    ds["val"] = ds.pop("test")  # type: ignore
+    try: 
+        ds["val"] = ds.pop("test")  # type: ignore
+    except KeyError:
+        ds["val"] = ds.pop("validation")
     print(ds)  # should show Dataset with 1000 rows
     return ds
 
