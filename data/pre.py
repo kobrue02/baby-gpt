75b833f18b473af34cce79c92c73383067d9343a
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2025-10-07 20:19:19 +0200

    fix sharding logic

--- data/pre.py
+++ data/pre.py
@@ -12,17 +12,15 @@ from tqdm import tqdm
 
 
 
-def get_dataset_splits(dataset_key, n_shards, test_size=0.001, seed=42):
+def get_dataset_splits(dataset_key, n_items, test_size=0.001, seed=42):
     print(f"Loading first {n_shards} shards from {dataset_key} dataset...")
     # Load dataset in streaming mode to get first n_shards worth of data
     # Stream the dataset and take only what we need
     ds = load_dataset(dataset_key, split="train", streaming=True)
-    shard_size = 10000  # Each shard has approximately 10,000 examples
     # Calculate approximate number of examples to take based on shard count
     # Each shard has roughly similar number of examples
     # We'll take the first portion of the dataset
-    total_examples = n_shards * shard_size
-    ds_list = list(tqdm(ds.take(total_examples), total=total_examples, desc="Loading examples"))
+    ds_list = list(tqdm(ds.take(n_items), total=n_items, desc="Loading examples")) # type: ignore
     ds = Dataset.from_list(ds_list)
     # Remove all columns except 'text'
     ds = ds.remove_columns([col for col in ds.column_names if col != 'text'])
