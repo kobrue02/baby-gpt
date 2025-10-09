5fab0cf51ab871055f1913c04fa3521ee6bd88ba
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2025-10-09 20:02:56 +0200

    ignore some typing warnings

--- training/pretraining/components/yarn.py
+++ training/pretraining/components/yarn.py
@@ -82,11 +82,7 @@ class LlamaYaRNScaledRotaryEmbedding(torch.nn.Module):
 
         # Build here to make `torch.jit.trace` work.
         self.max_seq_len_cached = max_position_embeddings
-        t = torch.arange(
-            self.max_seq_len_cached,
-            device=self.inv_freq.device,
-            dtype=self.inv_freq.dtype,
-        )
+        t = torch.arange(self.max_seq_len_cached, device=self.inv_freq.device, dtype=self.inv_freq.dtype,) # type: ignore
         freqs = torch.einsum("i,j->ij", t, self.inv_freq)
         # Different from paper, but it uses a different permutation in order to obtain the same calculation
         emb = torch.cat((freqs, freqs), dim=-1)
@@ -106,12 +102,10 @@ class LlamaYaRNScaledRotaryEmbedding(torch.nn.Module):
     def forward(self, x, seq_len=None):
         # x: [bs, num_attention_heads, seq_len, head_size]
         # This `if` block is unlikely to be run after we build sin/cos in `__init__`. Keep the logic here just in case.
-        if seq_len > self.max_seq_len_cached:
+        if seq_len > self.max_seq_len_cached: # type: ignore
             self.max_seq_len_cached = seq_len
 
-            t = torch.arange(
-                self.max_seq_len_cached, device=x.device, dtype=self.inv_freq.dtype
-            )
+            t = torch.arange(self.max_seq_len_cached, device=x.device, dtype=self.inv_freq.dtype,) # type: ignore
             freqs = torch.einsum("i,j->ij", t, self.inv_freq)
             # Different from paper, but it uses a different permutation in order to obtain the same calculation
             emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
@@ -127,8 +121,8 @@ class LlamaYaRNScaledRotaryEmbedding(torch.nn.Module):
                 persistent=False,
             )
         return (
-            self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
-            self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
+            self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype), # type: ignore
+            self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype), # type: ignore
         )
 
     def yarn(self, device):
